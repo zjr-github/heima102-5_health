b2fe7d1b21375aa82f1ccdff0feba81439b0f8ba
LL <LL>
2020-11-06 16:20:10 +0800

    第二次

--- health_pojo/src/main/java/com/itheima/health/entity/Result.java
+++ health_pojo/src/main/java/com/itheima/health/entity/Result.java
@@ -1,17 +1,11 @@
 package com.itheima.health.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
 /**
  * 封装返回结果
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Result implements Serializable {
     private boolean flag;//执行结果，true为执行成功 false为执行失败
     private String message;//返回结果信息
@@ -20,5 +14,32 @@ public class Result implements Serializable {
     public Result(boolean flag, String message) {
         this.flag = flag;
         this.message = message;
+    }
+
+    public Result(boolean flag, String message, Object data) {// 查询回显
+        this.flag = flag;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 }
